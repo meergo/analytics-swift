de73c724f643225cdf82d22fef71807f9eb6aaab
retini <andrea.retini@gmail.com>
2026-03-17 12:31:17 +0100

    Update the basic example app to test sessions and strategies

--- Examples/apps/BasicExample/BasicExample/EventData.swift
+++ Examples/apps/BasicExample/BasicExample/EventData.swift
@@ -9,46 +9,42 @@ import Foundation
 import Meergo
 import UIKit
 
-struct UserTraits: Codable {
+struct WorkoutProperties: Codable {
+    let workoutType: String
+    let durationMinutes: Int
+    let caloriesBurned: Int
+    let device: String
+}
+
+struct NameTraits: Codable {
+    let name: String
+}
+
+struct EmailTraits: Codable {
     let email: String
-    let birthday: String
-    let likesPho: Bool
 }
 
-struct TrackProperties: Codable {
-    let dayOfWeek: String
-}
-
-struct ScreenProperties: Codable {
-    let appUsage: TimeInterval
-}
-
-
-extension Date {
-    func dayOfWeek() -> String? {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "EEEE"
-        return dateFormatter.string(from: self).capitalized
-    }
+struct AgeTraits: Codable {
+    let age: String
 }
 
 
 class OutputPlugin: Plugin {
     let type: PluginType = .after
     let name: String
-    
+
     var analytics: Analytics?
     var textView: UITextView!
-    
+
     required init(name: String) {
         self.name = name
     }
-    
+
     init(textView: UITextView!) {
         self.textView = textView
         self.name = "output_capture"
     }
-    
+
     func execute<T>(event: T?) -> T? where T : RawEvent {
         let string = event?.prettyPrint()
         textView.text = string
--- Examples/apps/BasicExample/BasicExample/ViewController.swift
+++ Examples/apps/BasicExample/BasicExample/ViewController.swift
@@ -12,49 +12,46 @@ class ViewController: UIViewController {
     var analytics: Analytics? {
         return UIApplication.shared.delegate?.analytics
     }
-    
-    var usage: TimeInterval = 0
-    var timer: Timer? = nil
 
     @IBOutlet weak var eventView: UITextView!
-    
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        timer = Timer.scheduledTimer(withTimeInterval: 1, repeats: true) { _ in
-            self.usage += 1
-        }
-        
+
         let outputCapture = OutputPlugin(textView: eventView)
         analytics?.add(plugin: outputCapture)
     }
 
-    @IBAction func trackTapped(_ sender: Any) {
-        guard let day = Date().dayOfWeek() else {
-            print("we couldn't get the day of the week.  the world has ended. :(")
-            return
-        }
-        
-        let props = TrackProperties(dayOfWeek: day)
-        analytics?.track(name: "Track Tapped", properties: props)
+    @IBAction func startSessionTapped(_ sender: Any) {
+        analytics?.startSession()
     }
-    
-    @IBAction func screenTapped(_ sender: Any) {
-        let props = ScreenProperties(appUsage: usage)
-        analytics?.screen(title: "Main Screen", category: "Best", properties: props)
+
+    @IBAction func endSessionTapped(_ sender: Any) {
+        analytics?.endSession()
     }
-    
+
+    @IBAction func trackWorkoutTapped(_ sender: Any) {
+        analytics?.track(name: "Workout Completed", properties: WorkoutProperties(
+            workoutType: "Cardio",
+            durationMinutes: 45,
+            caloriesBurned: 380,
+            device: "Smartwatch"
+        ))
+    }
+
     @IBAction func identifyTapped(_ sender: Any) {
-        let traits = UserTraits(email: "sloth@meergo.com", birthday: "09/01/2011", likesPho: true)
-        analytics?.identify(userId: "sloth", traits: traits)
+        analytics?.identify(userId: "user1234", traits: NameTraits(name: "Jhon"))
     }
-    
-    @IBAction func groupTapped(_ sender: Any) {
-        analytics?.group(groupId: "1234")
+
+    @IBAction func identifyWithEmailTapped(_ sender: Any) {
+        analytics?.identify(userId: "", traits: EmailTraits(email: "user1234@example.com"))
     }
-    
-    @IBAction func flushTapped(_ sender: Any) {
-        analytics?.flush()
+
+    @IBAction func identifyWithAgeTapped(_ sender: Any) {
+        analytics?.identify(userId: "", traits: AgeTraits(age: "35"))
+    }
+
+    @IBAction func resetTapped(_ sender: Any) {
+        analytics?.reset()
     }
 }
-
